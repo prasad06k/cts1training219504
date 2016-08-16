3c04aaf662eed549668a3ebb7de63e4ec01b39e8
prasad06k <prasad06k@gmail.com>
2016-08-16 19:38:51 +0530

    web service

--- WebApplication1/WebApplication1/test.aspx.cs
+++ WebApplication1/WebApplication1/test.aspx.cs
@@ -7,13 +7,17 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using S = System.Data.SqlClient;
 
+
 namespace WebApplication1
 {
     public partial class test : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Label1.Text = "hello world";
+            WebService1 Service1 = new WebService1();
+
+
+            Label1.Text = Service1.HelloWorld(); 
 
             using (var connection = new S.SqlConnection(
                    "Server=tcp:dab03lchni.database.windows.net,1433;Initial Catalog=SqlCTS219504;Persist Security Info=False;User ID=saadmin;Password=password-1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
