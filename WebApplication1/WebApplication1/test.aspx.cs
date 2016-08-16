f0271042dff81777b0aebe829267ea32cb4cafbf
prasad06k <prasad06k@gmail.com>
2016-08-16 19:00:58 +0530

    added ado.net code

--- WebApplication1/WebApplication1/test.aspx.cs
+++ WebApplication1/WebApplication1/test.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using S=System.Data.SqlClient;
 
 namespace WebApplication1
 {
@@ -12,6 +13,17 @@ namespace WebApplication1
         protected void Page_Load(object sender, EventArgs e)
         {
             Label1.Text = "hello world";
+
+            using (var connection = new S.SqlConnection(
+                   "Server=tcp:dab03lchni.database.windows.net,1433;Initial Catalog=SqlCTS219504;Persist Security Info=False;User ID=saadmin;Password=password-1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+                   ))
+            {
+                connection.Open();
+                Label1.Text = "Connected successfully.";
+
+//                Console.WriteLine("Press any key to finish...");
+  //              Console.ReadKey(true);
+            }  
         }
     }
 }
